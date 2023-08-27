073252a3ffc5cf5705491f9a0634f6c7abd5bc0e
Chicguide <shdsamui99@gmail.com>
2023-08-27 15:42:05 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  serverUrl: 'https://nearmev5.quanlabs.com/api',
+  // serverUrl: 'https://nearmev5.quanlabs.com/api',
+  serverUrl: 'https://chic-guide-thailand-api-a2qdj.ondigitalocean.app/api',
   appId: 'ChicGuidThailand',
   languages: [{
     id: 'en',
